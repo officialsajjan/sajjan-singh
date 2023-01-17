866509d4e274b70468e42aa0cd93f69c2ddfa80c
sajjan@972308 <sajjanjasawat@gmail.com>
2023-01-17 15:18:14 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import ForgotPassoword from "./pages/ForgotPassoword";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Header from "./components/Header";
 function App() {
   return (
-    <h1 className="sm:bg-yellow-600">hello</h1>
+    <>
+    <Router>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="ForgotPassoword" element={<ForgotPassoword />}/>
+        <Route path="Offers" element={<Offers />}/>
+        <Route path="Signup" element={<Signup />}/>
+        <Route path="Signin" element={<Signin />}/>
+      </Routes>
+
+    </Router>
+    </>
   );
 }
 
---
+++ src/pages/ForgotPassoword.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ForgotPassoword() {
+  return (
+    <div>ForgotPassoword</div>
+  )
+}
