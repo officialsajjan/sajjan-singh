21f3b003c5cd9d4d76e34c38d48bf58deb2fc405
sajjan@972308 <sajjanjasawat@gmail.com>
2023-01-18 13:18:46 +0530

    changes-3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,53 @@
 import React from 'react'
+import {useLocation,useNavigate} from 'react-router-dom'
 
 export default function Header() {
+  const location= useLocation()
+  const navigate=useNavigate()
+  function show(route){
+    if(route==location.pathname){
+      return true
+    }
+  }
   return (
     <div className='bg-white border-b-2 shadow-sm sticky top-0 z-11'>
         <header className='flex justify-between items-center px-5 max-w-6xl mx-auto'>
         <div>
             <img src="https://b2bweb.realtor.com/web_assets/rdc/SFMC-lp-images/2021/RDC%20-tagline-wide.png"
             alt='ReactApp'
-            className='h-5'
+            className='h-5 cursor-pointer'
+            onClick={ ()=>navigate('/')}
+          
             />
 
         </div>
         <div>
             <ul className='flex space-x-6'>
-                <li className='py-3 text-slate-500 text-sm font-semibold'>Home</li>
-                <li className='py-3 text-slate-500 text-sm font-semibold'>Offers</li>
-                <li className='py-3 text-slate-500 text-sm font-semibold'>Signin</li>
+                <li 
+                className={`py-3 text-slate-500 text-sm font-semibold border-b-[3px] cursor-pointer
+                border-b-transparent ${show('/') && 'text-black border-b-red-600'}
+                `}
+                onClick={ ()=>navigate('/')}
+                >
+
+                  Home</li>
+                  <li 
+                className={`py-3 text-slate-500 text-sm font-semibold border-b-[3px] cursor-pointer
+                border-b-transparent ${show('/offers') && 'text-black border-b-red-600'}
+                `}
+                onClick={ ()=>navigate('/Offers')}
+                >
+
+                  Offers</li>
+                  <li 
+                className={`py-3 text-slate-500 text-sm font-semibold border-b-[3px] cursor-pointer
+                border-b-transparent ${show('/Signin') && 'text-black border-b-red-600'}
+                `}
+                onClick={ ()=>navigate('/Signin')}
+                >
+
+                  Signin</li>
+                
             </ul>
         </div>
         </header>
